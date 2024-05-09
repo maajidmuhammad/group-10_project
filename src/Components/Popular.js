6fafe3a0bd15dd6918feed9742579d797c25e7bd
maajidmuhammad <abdimohamed0742@gmail.com>
2024-05-09 14:25:10 +0300

    finising up the project

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -1,28 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { useGlobalContext } from '../context/global'
-import styled from 'styled-components'
-import Sidebar from './Sidebar'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/global';
+import styled from 'styled-components';
+import Sidebar from './Sidebar';
 
-function Popular({rendered}) {
-    const {popularAnime,isSearch, searchResults} = useGlobalContext()
-    
+function Popular({ rendered }) {
+    const { popularAnime, isSearch, searchResults } = useGlobalContext();
 
     const conditionalRender = () => {
-        if(!isSearch && rendered === 'popular'){
-            return popularAnime?.map((anime) => {
-                return <Link to={/anime/${anime.mal_id}} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
-        }else{
-            return searchResults?.map((anime) => {
-                return <Link to={/anime/${anime.mal_id}} key={anime.mal_id}>
-                    <img src={anime.images.jpg.large_image_url} alt="" />
-                </Link>
-            })
-        }
-    }
+        const animeData = isSearch ? searchResults : popularAnime;
+        return animeData?.map((anime) => (
+            <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+                <img src={anime.images?.jpg?.large_image_url} alt="" />
+            </Link>
+        ));
+    };
 
     return (
         <PopularStyled>
@@ -31,12 +23,12 @@ function Popular({rendered}) {
             </div>
             <Sidebar />
         </PopularStyled>
-    )
+    );
 }
 
 const PopularStyled = styled.div`
     display: flex;
-    .popular-anime{
+    .popular-anime {
         margin-top: 2rem;
         padding-top: 2rem;
         padding-bottom: 2rem;
@@ -48,18 +40,18 @@ const PopularStyled = styled.div`
         grid-gap: 2rem;
         background-color: #fff;
         border-top: 5px solid #e5e7eb;
-        a{
+        a {
             height: 500px;
             border-radius: 7px;
             border: 5px solid #e5e7eb;
-        }
-        a img{
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
-            border-radius: 5px;
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+                border-radius: 5px;
+            }
         }
     }
 `;
 
-export default Popular
+export default Popular;
